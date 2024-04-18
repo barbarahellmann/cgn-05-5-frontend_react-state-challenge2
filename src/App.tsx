0afa71623857f8b10e4fef8097e884e5b3cb59cb
barbarahellmann <babshellmann@web.de>
2024-04-18 16:32:29 +0200

    Integrated search

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,35 @@
-import { useState } from 'react'
+import {ChangeEvent, useState} from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import {response} from "./Response.ts";
 import Character, {CharacterProps} from "./Character.tsx";
+import {Simulate} from "react-dom/test-utils";
+import input = Simulate.input;
 
 function App() {
   const [characterData, setCharacterData]= useState<CharacterProps[]>(response.results)
+  const [searchName, setSearchName] = useState<string>("")
 
 
+function handleChange(event:ChangeEvent<HTMLInputElement>) {
+      setSearchName(event.target.value)
+}
 
+
+//filter den suchbegriff
+const filteredChars=characterData.filter(
+    (char) => char.name.toLowerCase().includes(searchName.toLowerCase())
+)
 
   return (
       <>
         <input
                 type="text"
-                placeholder="Comming soon"
-  //              value={}
-  //              onChange={}
+                placeholder="Search your Character by name"
+                onChange={handleChange}
         />
-        {characterData.map((characterTemp) => (
+        {filteredChars.map((characterTemp) => (
             <Character id={characterTemp.id} name={characterTemp.name} status={characterTemp.status} species={characterTemp.species}/>))}
       </>
   )
